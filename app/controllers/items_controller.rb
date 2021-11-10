0f3436c51480a3fe4d8e8a6a8c7b217c0f316c05
arahito1994 <artht0408@gmail.com>
2021-11-10 18:19:04 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except:[:index, :show]
-
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
   end
-
 end
