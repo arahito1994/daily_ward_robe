6b2dbbb0a35bafe0f6d4aaf6db82957cd0e96ba3
arahito1994 <artht0408@gmail.com>
2021-11-10 21:28:39 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
-  has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :style_text
+    validates :image
+  end
+  
+
+  
 end
