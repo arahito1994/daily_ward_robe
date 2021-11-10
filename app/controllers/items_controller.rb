d5c1daef2ed41b8c960b7316e03f0f7503ea7f5d
arahito1994 <artht0408@gmail.com>
2021-11-10 23:24:02 +0900

    newアクションとcreateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,17 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
   private
   
   def item_params
-    params.require(:item), permit(:image)
+    #params.require(:item), permit(:image)
   end
 
-  
+
 end
