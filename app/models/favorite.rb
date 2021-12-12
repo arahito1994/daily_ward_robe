0b6559a6971a6737ca5134acbc6152c3950a59ec
arahito1994 <artht0408@gmail.com>
2021-12-12 23:16:56 +0900

    Favoriteモデルの単体テスト

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :item
 
-  validates_uniquness_of :item_id, scope: :user_id
+  validates_uniqueness_of :item_id, scope: :user_id
 end
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @favorite = FactoryBot.build :favorite
+  end
+    describe 'いいね情報の保存' do
+      context 'いいねができるとき' do
+        it '必要情報が揃っていればいいねができる' do
+          expect(@favorite). to be_valid
+        end
+      end
+      context 'いいねができないとき' do
+        it 'userが紐づいていないといいねができない' do
+          @favorite.user = nil
+          @favorite.valid?
+          expect(@favorite.errors.full_messages).to include('User must exist')
+        end
+        it 'itemが紐づいていないといいねができない' do
+          @favorite.item = nil
+          @favorite.valid?
+          expect(@favorite.errors.full_messages).to include('Item must exist')
+        end
+      end
+    end
 end
